cb9d1c29af4365c6e5066a2e599db75a9be6c863
Matthew Campbell <cattmampbell@gmail.com>
2018-12-08 21:08:38 -0500

    Completed all objectives via app.js file!

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return new Promise((resolve, reject) => {
             // Adds 6 by 2
             resolve(slowMath.add(6, 2));
+            // resolve(slowMath.add(1, 1));
             reject(new Error('Oops! Something went wrong with Task 1!'));
         }).then((result) => {
             // Should log, '6 + 2 = 8!', to the console
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 'font-size: 12px; font-weight: 900; color: #343a40;',
                 'font-size: 12px; font-weight: 500; color: #343a40;',
                 'font-size: 12px; font-weight: 400; color: #343a40;',
-                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.550);',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
                 'font-size: 12px; font-weight: 400; color: #343a40;'
             )
             return new Promise((resolve, reject) => {
@@ -44,7 +45,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 'font-size: 12px; font-weight: 900; color: #343a40;',
                 'font-size: 12px; font-weight: 500; color: #343a40;',
                 'font-size: 12px; font-weight: 400; color: #343a40;',
-                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.550);',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
                 'font-size: 12px; font-weight: 400; color: #343a40;'
             )
             return new Promise((resolve, reject) => {
@@ -60,7 +61,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 'font-size: 12px; font-weight: 900; color: #343a40;',
                 'font-size: 12px; font-weight: 500; color: #343a40;',
                 'font-size: 12px; font-weight: 400; color: #343a40;',
-                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.550);',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
                 'font-size: 12px; font-weight: 400; color: #343a40;'
             )
             return new Promise((resolve, reject) => {
@@ -76,8 +77,99 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 'font-size: 12px; font-weight: 900; color: #343a40;',
                 'font-size: 12px; font-weight: 500; color: #343a40;',
                 'font-size: 12px; font-weight: 400; color: #343a40;',
-                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.550);',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
                 'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+            return new Promise((resolve, reject) => {
+                // Adds 1 by 98
+                resolve(slowMath.add(result, 98));
+                reject(new Error('Oops! Something went wrong with Task 5!'));
+            })
+        }).then((result) => {
+            // Should log, '1 + 98 = 99', to the console
+            console.log(
+                `%c1 %c+%c 98%c = %c ${result} %c!`,
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 400; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
+                'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+            return new Promise((resolve, reject) => {
+                // Remainder of 99 and 2
+                resolve(slowMath.remainder(result, 2));
+                reject(new Error('Oops! Something went wrong with Task 6!'));
+            })
+        }).then((result) => {
+            // Should log, '99 % 2 = 1', to the console
+            console.log(
+                `%c99%c % %c 2%c = %c ${result} %c!`,
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 400; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
+                'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+            return new Promise((resolve, reject) => {
+                // Multiplies 1 by 50 
+                resolve(slowMath.multiply(result, 50));
+                reject(new Error('Oops! Something went wrong with Task 7!'));
+            })
+        }).then((result) => {
+            // Should log, '1 * 50 = 50', to the console
+            console.log(
+                `%c1 %c*%c 50%c = %c ${result} %c!`,
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 400; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
+                'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+            return new Promise((resolve, reject) => {
+                // Remainder of 50 and 40
+                resolve(slowMath.remainder(result, 40));
+                reject(new Error('Oops! Something went wrong with Task 8!'));
+            })
+        }).then((result) => {
+            // Should log, '50 % 40 = 10', to the console
+            console.log(
+                `%c50%c % %c40%c = %c ${result} %c!`,
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 400; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.375); border-radius: 12px;',
+                'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+            return new Promise((resolve, reject) => {
+                // Adds 10 by 32
+                resolve(slowMath.add(result, 32));
+                reject(new Error('Oops! Something went wrong with Task 9!'));
+            })
+        }).then((result) => {
+            // Should log, '10 + 32 = 42', to the console
+            console.log(
+                `%cThe final result is: %c 10%c + %c32%c = %c ${result} %c!`,
+                'font-size: 12px; font-weight: 400; color: #343a40; background-color: rgba(248, 249, 250, 0.975); border-radius: 12px;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40;',
+                'font-size: 12px; font-weight: 500; color: #343a40;',
+                'font-size: 12px; font-weight: 400; color: #343a40;',
+                'font-size: 12px; font-weight: 900; color: #343a40; background-color: rgba(255, 193, 7, 0.250); border-radius: 12px;',
+                'font-size: 12px; font-weight: 400; color: #343a40;'
+            )
+        }).catch((error) => {
+            console.log(
+                `%cRut-roh%c! Looks like an error occurred!`,
+                'font-size: 12px; font-weight: 900; color: #dc3545; background-color: rgba(220, 53, 69, 0.125); border-radius: 12px;',
+                'font-size: 12px; font-weight: 600; color: #dc3545; background-color: rgba(220, 53, 69, 0.125); border-radius: 12px;'
+            )
+            console.log(
+                `%c${error}`,
+                'font-size: 12px; font-weight: 400; color: #dc3545;'
             )
         })
     }
